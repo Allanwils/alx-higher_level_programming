c1ad5d7bbe38e9bc19b7e0db66142fd12adaf019
Allan Masiga <zionalehebrew@gmail.com>
2023-03-13 08:06:40 -0700

    Node4 code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const numbers = args.map(Number);
-
-if (numbers.length === 0) {
-console.log(0);
-} else if (numbers.length === 1) {
-console.log(0);
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-const sortedNumbers = numbers.sort((a, b) => b - a);
-console.log(sortedNumbers[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
